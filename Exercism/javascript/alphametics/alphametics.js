a7a702c248ea3757ccd79b7bf98a9941e2dcf19e
Thewessen <sthewessen@gmail.com>
2019-07-11 23:18:01 +0200

    Solution to alphametic solver added

--- Exercism/javascript/alphametics/alphametics.js
+++ Exercism/javascript/alphametics/alphametics.js
@@ -1,30 +1,43 @@
 'use strict'
 
-const letters = (str) => new Set(str.match(/[A-Z]/g))
-const words = (str) => str.match(/[A-Z]+/g)
+const words = /[A-Z]+/g
+const chars = /[A-Z]/g
+const letters = (str) => Array.from(new Set(str.match(chars)))
+const firstLetters = (str) => new Set(str.replace(words, (w) => w[0]))
+const numbers = (to) => Array.from({ length: to }, (__, i) => i)
 
-const combine = function* (letter, scores) {
-  for (const score of scores) {
-    yield [letter, score]
-  }
-}
-
-const solutionGen = function* (letters, scores, solution = {}) {
+const solutionGen = function* (letters, scores, notZero, solution) {
+  notZero = notZero || new Set()
+  solution = solution || {}
   const letter = letters.pop()
-  for (const score of scores) {
+  const scrs = notZero.has(letter) ? scores.filter((s) => s !== 0) : scores
+  for (const score of scrs) {
     solution[letter] = score
-    console.log(solution)
     if (letters.length === 0) {
       yield solution
     } else {
-      yield* solutionGen(letters, scores.filter((s) => s !== score), solution)
-      yield solution
+      yield* solutionGen(
+        [...letters],
+        scores.filter((s) => s !== score),
+        notZero,
+        solution
+      )
     }
   }
 }
 
-module.exports = solutionGen
-
-const solve = (puzzle) => {
-  const l = letters(puzzle)
+export const solve = (puzzle) => {
+  const lets = letters(puzzle)
+  const scrs = numbers(10)
+  const notZero = firstLetters(puzzle)
+  let solution = null
+  for (const sol of solutionGen(lets, scrs, notZero)) {
+    if (eval(puzzle.replace(chars, (char) => sol[char]))) {
+      if (solution) {
+        return null
+      }
+      solution = { ...sol }
+    }
+  }
+  return solution
 }
