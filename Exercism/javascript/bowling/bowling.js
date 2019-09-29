4e42aaa02675d0350c7d8fb53b8d121cf42a8e59
Thewessen <sthewessen@gmail.com>
2019-09-29 17:11:40 +0200

    Completed bowling exercise Exercism

--- Exercism/javascript/bowling/bowling.js
+++ Exercism/javascript/bowling/bowling.js
@@ -8,105 +8,91 @@ const total = (scores) => sum(scores.map((f) => f.score))
 
 export class Frame {
   constructor() {
+    this.pins = PINCOUNT
     this._rolls = []
     this.bonus = 0
-    this._score = 0
+    this.last = false
   }
 
   get score() {
-    return this._score
+    return sum(this.rolls)
   }
 
-  set score(value) {
-    this._score = sum(this.rolls) + value
-  }
-
-  set rolls(pins) {
-    this._rolls = [...this._rolls, pins]
-  }
-  
   get rolls() {
     return this._rolls
   }
 
-  check() {
-    const check = this.rolls.length === THROWCOUNT
-      && this.score <= PINCOUNT
-    if ((this.rolls.length === THROWCOUNT
-        && this.score <= PINCOUNT)
-      || (this.rolls.length === THROWCOUNT + 1() {
+  roll(pins) {
+    this.pins -= pins
+    if (this.pins < 0) {
       throw new Error('Pin count exceeds pins on the lane')
     }
+    if (this.last && !this.pins) {
+      this.pins = PINCOUNT
+    }
+    this._rolls.push(pins)
   }
-  // reset() {
-  //   this.pins = PINCOUNT
-  // }
+  
+  addBonus(pins) {
+    if (this.bonus) {
+      this.rolls.push(pins)
+      this.bonus -= 1
+    }
+  }
+
+  setLast() {
+    this.last = true
+    this.pins = PINCOUNT
+    return this
+  }
+
+  * play (total) {
+    if (this.rolls.length === THROWCOUNT) {
+      return this
+    }
+    this.roll(yield total + this.score)
+    if (this.score === PINCOUNT) {
+      this.bonus = this.rolls.length % 2 ? 2 : 1
+      return this
+    }
+    return yield * this.play(total)
+  }
 }
 
 export class Bowling {
   constructor () {
-    this.game = Bowling.start()
+    this.game = Bowling.play()
     Object.assign(this, this.game.next())
-  }
-
-  static check (pins) {
-    if (pins > PINCOUNT) {
-      throw new Error('Pin count exceeds pins on the lane')
-    }
-    return pins
-  }
-
-  static * rolls (frame, total) {
-    if (frame.rolls.length === THROWCOUNT) {
-      return frame
-    }
-    frame.rolls = yield total + frame.score
-    frame.check()
-    if (frame.score === PINCOUNT) {
-      frame.bonus = frame.rolls.length % 2 ? 2 : 1
-      return frame
-    }
-    return yield * Bowling.rolls(frame, total)
   }
 
   static * frames (scores, bonus) {
     if(scores.length === FRAMECOUNT) {
       return scores
     }
-    const frame = yield * Bowling.rolls(new Frame(), total(scores))
-    let bonusFrames = scores.filter(frame => frame.bonus)
+    const frame = yield * new Frame().play(total(scores))
     for (const roll of frame.rolls) {
-      bonusFrames = bonusFrames.map(frame => {
-        frame.rolls = roll
-        frame.bonus -= 1
-        return frame
-      }).filter(frame => frame.bonus)
+      scores.forEach(frame => frame.addBonus(roll))
     }
     return yield * Bowling.frames([...scores, frame], bonus)
   }
 
-  static * start () {
+  static * play () {
     // Regular frames
-    let scores = yield * Bowling.frames([], 0)
-    console.log(scores)
+    const scores = yield * Bowling.frames([], 0)
 
-    // Fill balls
-    let bonusFrames = scores.filter(frame => frame.bonus)
-    let pins = 0
-    for (let i = 0; i < 2 && bonusFrames.length > 0; i += 1) {
-      let roll = yield total(scores)
-      Bowling.check(pins += roll)
-      bonusFrames = bonusFrames.map(frame => {
-        frame.rolls = pins
-        frame.bonus -= 1
-        console.log(frame)
-        return frame
-      }).filter(frame => frame.bonus)
-      if (pins === PINCOUNT) {
-        pins = 0
-      }
+    // Last frame
+    const lastFrame = scores.pop().setLast()
+    if (lastFrame.rolls.length < 2) {
+      const pins = yield total([...scores, lastFrame])
+      lastFrame.roll(pins)
+      scores.forEach(frame => frame.addBonus(pins))
     }
-    return total(scores)
+
+    // fillball
+    if ([...scores, lastFrame].some(frame => frame.bonus)) {
+      lastFrame.roll(yield total([...scores, lastFrame]))
+    }
+    return total([...scores, lastFrame])
   }
 
   roll (pins) {
