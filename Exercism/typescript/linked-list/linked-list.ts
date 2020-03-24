bca6a259d4ddb4b8a2e9757e6d7a89c3f7e22f16
Thewessen <sthewessen@gmail.com>
2020-03-24 15:54:47 +0100

    Typescript linked list completed

--- Exercism/typescript/linked-list/linked-list.ts
+++ Exercism/typescript/linked-list/linked-list.ts
@@ -1,56 +1,100 @@
-export class LinkedList<T> {
+class LinkNode<T> {
   constructor (
-    public value: T,
-    public previous: LinkedList<T> = null,
-    public next: LinkedList<T> = null,
+    private value: T,
+    private previous: LinkNode<T> | null = null,
+    private next: LinkNode<T> | null = null,
+  ) {}
+  
+  getValue (): T {
+    return this.value
+  }
+
+  getPrevious (): LinkNode<T> | null {
+    return this.previous
+  }
+
+  setPrevious (value: LinkNode<T> | null): void {
+    this.previous = value
+  }
+
+  getNext (): LinkNode<T> | null {
+    return this.next
+  }
+
+  setNext (value: LinkNode<T> | null): void {
+    this.next = value
+  }
+}
+
+export default class LinkedList<T> {
+  constructor (
+    private head: LinkNode<T> | null = null,
   ) {}
 
-  // last () {
-  //   return this.data[this.data.length - 1] || null
-  // }
+  last (): LinkNode<T> | null {
+    let lastNode = this.head
+    while (
+      lastNode instanceof LinkNode &&
+      lastNode.getNext() instanceof LinkNode
+    ) {
+      lastNode = lastNode.getNext()
+    }
+    return lastNode
+  }
 
-  // first () {
-  //   return this.data[0] || null
-  // }
+  push (value: T): void {
+    const lastNode = this.last()
+    if (lastNode instanceof LinkNode) {
+      lastNode.setNext(new LinkNode(value, lastNode, null))
+    } else {
+      this.head = new LinkNode(value, null, null)
+    }
+  }
 
-  // push (value) {
-  //   const node = new LinkNode(
-  //     value, this.last(), null
-  //   )
-  //   if (this.data.length > 0) {
-  //     this.last().after = node
-  //   }
-  //   this.data.push(node)
-  //   return this.data.length
-  // }
+  pop (): T {
+    const lastNode = this.last()
+    if (lastNode === null) {
+      throw new Error('Empty list!')
+    }
+    const previous = lastNode.getPrevious()
+    if (previous instanceof LinkNode) {
+      previous.setNext(null)
+    }
+    return lastNode.getValue()
+  }
 
-  // pop () {
-  //   const node = this.data.pop()
-  //   if (this.data.length > 0) {
-  //     this.last().after = null
-  //   }
-  //   return node.value
-  // }
+  shift (): T {
+    const firstNode = this.head
+    if (firstNode === null) {
+      throw new Error('List empty!')
+    }
 
-  // shift () {
-  //   const node = this.data.shift()
-  //   if (this.data.length > 0) {
-  //     this.first().before = null
-  //   }
-  //   return node.value
-  // }
+    const next = firstNode.getNext()
+    if (next instanceof LinkedList) {
+      next.setPrevious(null)
+    }
 
-  // unshift (value) {
-  //   const node =  new LinkNode(
-  //     value, null, this.first()
-  //   )
-  //   if (this.data.length > 0) {
-  //     this.first().before = node
-  //   }
-  //   this.data.unshift(node)
-  //   return this.data.length
-  // }
+    this.head = next
+    return firstNode.getValue()
+  }
 
+  unshift (value: T): void {
+    const newNode = new LinkNode(value, null, this.head)
+    if (this.head instanceof LinkNode) {
+      this.head.setPrevious(newNode)
+    }
+    this.head = newNode
+  }
+
+  count(): number {
+    let count = 0
+    let node = this.head
+    while (node instanceof LinkNode) {
+      count += 1
+      node = node.getNext()
+    }
+    return count
+  }
   // delete (value) {
   //   const index = this.data.map(
   //       (node) => node.value
@@ -65,9 +109,5 @@ export class LinkedList<T> {
   //     }
   //     return this.data.length
   //   }
-  // }
-
-  // count() {
-  //   return this.data.length
   // }
 }
