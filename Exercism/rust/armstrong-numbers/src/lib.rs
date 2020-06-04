d2664e8b3d9492f89a23aa89f5fec596c7448423
Samuel Thewessen <s.thewessen@hypotheekbond.nl>
2020-06-04 16:53:39 +0200

    Some refactor to latest solution (armstrong numbers in rust)

--- Exercism/rust/armstrong-numbers/src/lib.rs
+++ Exercism/rust/armstrong-numbers/src/lib.rs
@@ -1,18 +1,7 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    armstrong_sum(&num) == num
-}
-
-fn armstrong_sum(num: &u32) -> u32 {
-    let digits = digits(num);
-    digits
-       .iter()
-       .map(|n| n.pow(digits.len() as u32))
-       .sum()
-}
-
-fn digits(num: &u32) -> Vec<u32> {
-    num.to_string()
-       .chars()
-       .map(|c| c.to_digit(10).unwrap_or(0))
-       .collect()
+    let string = num.to_string();
+    string.chars()
+          .map(|c| c.to_digit(10).unwrap_or(0))
+          .map(|n| n.pow(string.len() as u32))
+          .sum::<u32>() == num
 }
