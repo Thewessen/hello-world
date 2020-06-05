10c6704e52683fbfef6dcfd346822e2acd94e978
Thewessen <sthewessen@gmail.com>
2020-06-05 19:49:29 +0200

    Using a crate from a different location on my pc for completing exercise

--- Exercism/rust/prime-factors/src/lib.rs
+++ Exercism/rust/prime-factors/src/lib.rs
@@ -1,18 +1,15 @@
-mod primes;
-
-use crate::primes::Primes;
+use primes::Primes;
 
 pub fn factors(n: u64) -> Vec<u64> {
     let mut factors: Vec<u64> = Vec::new();
     let primes = Primes::new();
-    // let boundry = n / 2;
     let mut number = n;
-    for prime in primes {
+    for prime in primes.take_while(|p| p <= &n) {
         while number % prime == 0 {
             number /= prime;
             factors.push(prime);
         }
-        if prime > number {
+        if number == 1 {
             break;
         }
     }
