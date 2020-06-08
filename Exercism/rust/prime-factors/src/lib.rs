8f09ee91e1486cb7447ba15187fc022cb6e1f7d5
Thewessen <sthewessen@gmail.com>
2020-06-08 19:26:03 +0200

    Rust prime factor exercise added

--- Exercism/rust/prime-factors/src/lib.rs
+++ Exercism/rust/prime-factors/src/lib.rs
@@ -1,16 +1,13 @@
-use primes::Primes;
-
 pub fn factors(n: u64) -> Vec<u64> {
     let mut factors: Vec<u64> = Vec::new();
-    let primes = Primes::new();
-    let mut number = n;
-    for prime in primes.take_while(|p| p <= &n) {
-        while number % prime == 0 {
-            number /= prime;
-            factors.push(prime);
-        }
-        if number == 1 {
-            break;
+    let mut rem = n;
+    let mut div = 2;
+    while rem > 1 {
+        if let 0 = rem % div {
+            factors.push(div);
+            rem /= div;
+        } else {
+            div += 1;
         }
     }
     factors
