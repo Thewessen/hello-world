7a4f9c72887e6fcf6cd676465a132d1d425f7f29
Thewessen <sthewessen@gmail.com>
2020-06-08 19:24:23 +0200

    Added new Rust book chapters (inc workspace)

--- Rust/if_let/src/main.rs
+++ Rust/if_let/src/main.rs
@@ -1,3 +1,25 @@
+#![allow(dead_code)]
 fn main() {
     println!("Hello, world!");
+}
+
+fn impl_with_match(o: Option<i32>) -> i32 {
+    match o {
+        Some(3) => 3,
+        _ => 0
+    }
+}
+
+fn impl_with_if_let(o: Option<i32>) -> i32 {
+    let mut r = 0;
+    if let Some(3) = o {
+        r = 3;
+    }
+    r
+}
+
+#[test]
+fn both_impl_are_the_same() {
+    let input = Some(3);
+    assert_eq!(impl_with_match(input), impl_with_if_let(input));
 }
