0ca68713274fd8a01b69fd8518f3e51212415eb5
Thewessen <sthewessen@gmail.com>
2020-12-12 16:54:02 +0100

    Clean up code from day02

--- Adventofcode/2020/day02/src/main.rs
+++ Adventofcode/2020/day02/src/main.rs
@@ -2,7 +2,7 @@ use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
 use structopt::StructOpt;
 use std::fs::File;
-use regex::Regex;
+use std::convert::TryFrom;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
@@ -20,14 +20,13 @@ fn main() -> io::Result<()> {
     let toboggan = args.toboggan;
     let result = reader
         .lines()
-        .map(|line| line.unwrap_or(String::new()))
-        .filter(|line| !line.is_empty())
-        .map(|line| (Policy::from_str(&line), get_password(&line)))
-        .fold(0, |r, (p, s)| {
+        .filter_map(|line| line.ok())
+        .filter_map(|line| Password::try_from(line.as_ref()).ok())
+        .fold(0, |r, p| {
             if toboggan {
-                r + (p.is_valid_toboggan(&s) as u16)
+                r + (p.is_valid_toboggan() as u16)
             } else {
-                r + (p.is_valid(&s) as u16)
+                r + (p.is_valid() as u16)
             }
         });
 
@@ -36,50 +35,65 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct Policy {
     letter: char,
-    lower_bound: u16,
-    upper_bound: u16
+    lower_bound: usize,
+    upper_bound: usize
 }
 
-impl Policy {
-    pub fn from_str(s: &str) -> Self {
-        let re = Regex::new(r"^(\d+)-(\d+) (\w)").unwrap();
-        let caps = re.captures(s).unwrap();
-        Policy {
-            letter: caps.get(3).unwrap().as_str().chars().next().unwrap(),
-            lower_bound: caps.get(1).unwrap().as_str().parse::<u16>().unwrap(),
-            upper_bound: caps.get(2).unwrap().as_str().parse::<u16>().unwrap(),
-        }
-    }
+#[derive(Debug, PartialEq, Eq)]
+struct Password {
+    value: String,
+    policy: Policy,
+}
 
-    fn is_valid(&self, p: &str) -> bool {
-        let count = p.chars()
-            .filter(|ch| ch.to_owned() == self.letter)
-            .count() as u16;
-         self.lower_bound <= count && count <= self.upper_bound
-    }
+impl TryFrom<&str> for Password {
+    type Error = &'static str;
 
-    fn is_valid_toboggan(&self, p: &str) -> bool {
-        let mut chars = p.chars();
-        let first = chars.nth((self.lower_bound - 1) as usize).unwrap_or(' ');
-        let second = chars.nth((self.upper_bound - self.lower_bound - 1) as usize).unwrap_or(' ');
-        (first == self.letter) ^ (second == self.letter)
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        let mut parts = s.split(' ');
+        let mut bounds = parts.next()
+            .expect("string should not be empty")
+            .split('-')
+            .filter_map(|n| n.parse::<usize>().ok());
+        let letter = parts.next()
+            .and_then(|part| part.chars().next())
+            .expect("policy should contain a letter");
+        let value = parts.next()
+            .unwrap_or("")
+            .to_string();
+        let password = Password { value, policy: Policy {
+            letter,
+            lower_bound: bounds.next().unwrap_or(0),
+            upper_bound: bounds.next().unwrap_or(0),
+        }};
+        Ok(password)
     }
 }
 
-impl PartialEq for Policy {
-    fn eq(&self, other: &Self) -> bool {
-        self.letter == other.letter &&
-        self.lower_bound == other.lower_bound &&
-        self.upper_bound == other.upper_bound
+impl Password {
+    fn is_valid(&self) -> bool {
+        let count = self.value.chars()
+            .filter(|ch| ch == &self.policy.letter)
+            .count();
+         self.policy.lower_bound <= count && count <= self.policy.upper_bound
     }
-}
-impl Eq for Policy {}
 
-fn get_password(s: &str) -> String {
-    s.split(':').nth(1).unwrap().trim().to_owned()
+    fn is_valid_toboggan(&self) -> bool {
+        let mut chars = self.value.chars();
+        let first = match chars.nth((self.policy.lower_bound - 1) as usize) {
+            Some(chr) => chr,
+            None => return false,
+        };
+        let second = match chars.nth(
+            (self.policy.upper_bound - self.policy.lower_bound - 1) as usize
+        ) {
+            Some(chr) => chr,
+            None => return false,
+        };
+        (first == self.policy.letter) ^ (second == self.policy.letter)
+    }
 }
 
 #[cfg(test)]
@@ -87,77 +101,86 @@ mod tests {
     use super::*;
 
     #[test]
-    fn correct_policy_from_str() {
-        let p = Policy {
-            letter: 'a',
-            lower_bound: 1,
-            upper_bound: 3,
-        };
-        assert_eq!(Policy::from_str("1-3 a: asoeut"), p);
-    }
-
-    #[test]
     fn correct_password_from_str() {
-        assert_eq!(get_password("1-3 a: asoeut"), "asoeut");
+        let p = Password {
+            value: String::from("asoeut"),
+            policy: Policy {
+                letter: 'a',
+                lower_bound: 1,
+                upper_bound: 3,
+            }};
+        assert_eq!(Password::try_from("1-3 a: asoeut"), Ok(p));
     }
 
     #[test]
     fn is_valid_password() {
-        let p = Policy {
-            letter: 'a',
-            lower_bound: 1,
-            upper_bound: 3,
-        };
-        assert_eq!(p.is_valid("asoeut"), true);
+        let p = Password {
+            value: String::from("asoeut"),
+            policy: Policy {
+                letter: 'a',
+                lower_bound: 1,
+                upper_bound: 3,
+            }};
+        assert!(p.is_valid());
     }
 
     #[test]
     fn is_not_valid_password() {
-        let p = Policy {
-            letter: 'b',
-            lower_bound: 1,
-            upper_bound: 3,
-        };
-        assert_eq!(p.is_valid("asoeut"), false);
+        let p = Password {
+            value: String::from("asoeut"),
+            policy: Policy {
+                letter: 'b',
+                lower_bound: 1,
+                upper_bound: 3,
+            }};
+        assert_eq!(p.is_valid(), false);
     }
 
     #[test]
     fn is_first_valid_toboggan() {
-        let p = Policy {
-            letter: 's',
-            lower_bound: 2,
-            upper_bound: 4,
-        };
-        assert_eq!(p.is_valid_toboggan("asoeut"), true);
+        let p = Password {
+            value: String::from("asoeut"),
+            policy: Policy {
+                letter: 's',
+                lower_bound: 2,
+                upper_bound: 4,
+            }};
+        assert!(p.is_valid_toboggan());
     }
 
     #[test]
     fn is_second_valid_toboggan() {
-        let p = Policy {
-            letter: 'e',
-            lower_bound: 2,
-            upper_bound: 4,
-        };
-        assert_eq!(p.is_valid_toboggan("asoeut"), true);
+        let p = Password {
+            value: String::from("asoeut"),
+            policy: Policy {
+                letter: 'e',
+                lower_bound: 2,
+                upper_bound: 4,
+            }};
+        assert!(p.is_valid_toboggan());
     }
 
     #[test]
     fn is_first_not_valid_toboggan() {
-        let p = Policy {
-            letter: 'x',
-            lower_bound: 2,
-            upper_bound: 5,
-        };
-        assert_eq!(p.is_valid_toboggan("osoeut"), false);
+        let p = Password {
+            value: String::from("osoeut"),
+            policy: Policy {
+                letter: 'x',
+                lower_bound: 2,
+                upper_bound: 5,
+            }};
+        assert_eq!(p.is_valid_toboggan(), false);
     }
 
     #[test]
     fn is_second_not_valid_toboggan() {
-        let p = Policy {
-            letter: 'o',
-            lower_bound: 1,
-            upper_bound: 3,
-        };
-        assert_eq!(p.is_valid_toboggan("osoeut"), false);
+        let p = Password {
+            value: String::from("osoeut"),
+            policy: Policy {
+                letter: 'o',
+                lower_bound: 1,
+                upper_bound: 3,
+            }};
+        assert_eq!(p.is_valid_toboggan(), false);
     }
 }
