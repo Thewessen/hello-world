77fad3816f007c057450ed5a0ab4ca67ecc86a4c
Thewessen <sthewessen@gmail.com>
2020-12-12 22:13:49 +0100

    Day 5 cleaned

--- Adventofcode/2020/day05/src/main.rs
+++ Adventofcode/2020/day05/src/main.rs
@@ -6,7 +6,6 @@ use std::cmp::Ordering;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
-    // Input file
     #[structopt(parse(from_os_str))]
     file: PathBuf,
     #[structopt(short = "p", long = "part-two")]
@@ -17,51 +16,57 @@ fn main() -> io::Result<()> {
     let args = Cli::from_args();
     let file = File::open(args.file)?;
     let reader = BufReader::new(file);
-    let mut result = Seat::new(0);
-    let seats = reader
+
+    let mut seats = reader
         .lines()
-        .map(|line| line.unwrap_or(String::new()))
+        .filter_map(|line| line.ok())
         .filter(|line| !line.is_empty())
-        .map(|seat| Seat::from_binary_space(&seat));
+        .map(|seat| Seat::from_space(&seat));
 
-    if args.part_two {
-        let mut plain = (0..(1 << 11))
-            .map(|i| Seat::new(i))
-            .collect::<Vec<Seat>>();
-
-        // sort seats
-        seats.for_each(|seat| {
-            let id = seat.id as usize;
-            plain[id] = seat.to_owned();
-        });
-
-        let mut iter = plain.iter().skip_while(|seat| seat.free).peekable();
-        result = loop {
-            let seat = iter.next().unwrap();
-            if seat.free && !iter.peek().unwrap().free {
-                break seat.to_owned();
-            }
-        }
-    } else {
-        result = seats.fold(result, |a, b| {
+    let result = match args.part_two {
+        true => owned_seat(&mut seats),
+        false => seats.fold(Seat::default(), |a, b| {
             if a >= b { a } else { b }
-        });
-    }
-    println!("{:?}", result.id);
+        })
+    };
+
+    println!("{}", result.id);
     Ok(())
 }
 
-fn bin_from_space(s: &str) -> u32 {
-    let bin = s.chars()
-        .map(|ch| match ch {
-            'B' => '1',
-            'F' => '0',
-            'R' => '1',
-            'L' => '0',
-            _ => '0',
-        })
-        .collect::<String>();
-    u32::from_str_radix(&bin, 2).unwrap_or(0)
+fn id_from_space(s: &str) -> u32 {
+    s.chars()
+     .map(|ch| match ch {
+         'B' => '1',
+         'F' => '0',
+         'R' => '1',
+         'L' => '0',
+         _ => '0',
+     })
+     .filter_map(|b| b.to_digit(2))
+     .fold(0, |n, b| n * 2 + b)
+}
+
+fn owned_seat<I>(seats: &mut I) -> Seat
+where I: Iterator<Item = Seat>
+{
+    let mut plain = (0..(1 << 11))
+        .map(|i| Seat::new(i))
+        .collect::<Vec<Seat>>();
+
+    // sort seats
+    seats.for_each(|seat| {
+        let id = seat.id as usize;
+        plain[id] = seat.to_owned();
+    });
+
+    let mut iter = plain.iter().skip_while(|seat| seat.free).peekable();
+    loop {
+        let seat = iter.next().expect("available seat");
+        if seat.free && !iter.peek().expect("occupied seat").free {
+            break seat.to_owned();
+        }
+    }
 }
 
 #[derive(Eq, Debug)]
@@ -70,14 +75,20 @@ struct Seat {
     free: bool,
 }
 
+impl Default for Seat {
+    fn default() -> Seat {
+        Seat { id: 0, free: true }
+    }
+}
+
 impl Seat {
-    fn new(id: u32) -> Self {
+    fn new(id: u32) -> Seat {
         Seat { id, free: true }
     }
 
-    fn from_binary_space(s: &str) -> Self {
+    fn from_space(s: &str) -> Self {
         Seat {
-            id: bin_from_space(&s),
+            id: id_from_space(&s),
             free: false
         }
     }
@@ -114,10 +125,10 @@ mod tests {
 
     #[test]
     fn test_from_string() {
-        assert_eq!(bin_from_space("FBF"), 2);
-        assert_eq!(bin_from_space("BFB"), 5);
+        assert_eq!(id_from_space("FBF"), 2);
+        assert_eq!(id_from_space("BFB"), 5);
 
-        assert_eq!(bin_from_space("LRR"), 3);
-        assert_eq!(bin_from_space("RRL"), 6);
+        assert_eq!(id_from_space("LRR"), 3);
+        assert_eq!(id_from_space("RRL"), 6);
     }
 }
