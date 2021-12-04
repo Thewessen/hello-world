58d2951bc8c3fa5f94c65301361bc71ea8dab5e2
Thewessen <sthewessen@gmail.com>
2021-12-04 15:56:05 +0100

    Some minor improvements on previous (day4) puzzles

--- Adventofcode/2021/day1/main.py
+++ Adventofcode/2021/day1/main.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 from typing import Callable, Optional
 from collections.abc import Iterator
 from functools import reduce
-from more_itertools import take
+from more_itertools import triplewise
 
 
 def sum_increasing() -> Callable[[int, str], int]:
@@ -24,23 +24,16 @@ def count_increasing(measurements: Iterator) -> int:
     return reduce(sum_increasing(), measurements, 0)
 
 
-def window_sum(data: Iterator[str], size=1) -> Iterator[int]:
+def window_sum(data: Iterator[str]) -> Iterator[int]:
     """Sum the data for a given window"""
-    window = take(size, data) 
-    lines = enumerate(data) 
-
-    if len(window) == size:
-        yield sum(int(d) for d in window)
-
-    for index, line in lines:
-        window[index % size] = line
+    for window in triplewise(data):
         yield sum(int(d) for d in window)
 
 
-def count_windowed_increasing(measurements: Iterator[str], window_size: int) -> int:
+def count_windowed_increasing(measurements: Iterator[str]) -> int:
     """Count the number of times the sanitized measurement is increasing
     (see README part 2)"""
-    return count_increasing(window_sum(measurements, window_size))
+    return count_increasing(window_sum(measurements))
 
 
 def main():
@@ -53,7 +46,7 @@ def main():
     args = parser.parse_args()
     with open(args.path, 'r') as data:
         if args.part2:
-            print(count_windowed_increasing(data, 3))
+            print(count_windowed_increasing(data))
         else:
             print(count_increasing(data))
 
--- Adventofcode/2021/day3/main.py
+++ Adventofcode/2021/day3/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from typing import Iterator, Tuple
+from typing import Iterator
 from functools import reduce
-from more_itertools import nth
+from itertools import groupby
 
 def calc_bit_balance(acc: int, curr: str) -> int:
     """Reducer function to convert an iterable of bits to a positive or
@@ -13,7 +13,7 @@ def calc_bit_balance(acc: int, curr: str) -> int:
     return acc - 1
 
 
-def gamma_epsilon_bits(balance: int) -> Tuple[int, int]:
+def gamma_epsilon_bits(balance: int) -> tuple[int, int]:
     """Separates a given balance into gamma and epsilon bits resp."""
     if balance > 0:
         return 1, 0
@@ -23,7 +23,7 @@ def gamma_epsilon_bits(balance: int) -> Tuple[int, int]:
     exit(1)
 
 
-def calc_gamma_epsilon_bin(acc: Tuple[str, str], curr: Tuple[int, int]) -> Tuple[str, str]:
+def calc_gamma_epsilon_bin(acc: tuple[str, str], curr: tuple[int, int]) -> tuple[str, str]:
     """Reducer function converting a gamma_bit, epsilon_bit tuple iterable into their
     corresponding binary string tuple"""
     gamma, epsilon = acc
@@ -31,7 +31,7 @@ def calc_gamma_epsilon_bin(acc: Tuple[str, str], curr: Tuple[int, int]) -> Tuple
     return gamma + str(gb), epsilon + str(eb)
 
 
-def calc_gamma_epsilon_rate(data: Iterator[str]) -> Tuple[int, int]:
+def calc_gamma_epsilon_rate(data: Iterator[str]) -> tuple[int, int]:
     """Given lines of binary string, returns the gamma-, epsilon-rates as tuple."""
     bit_balance = (reduce(calc_bit_balance, position, 0)
                    for position in zip(*data))
@@ -43,34 +43,32 @@ def calc_gamma_epsilon_rate(data: Iterator[str]) -> Tuple[int, int]:
 def filter_by_criteria(data: list[str], criteria, poss=0) -> str:
     """With a given criteria for bit vs. corresponding bit_balance (1 or 0),
     keeps filtering until one line of data is left."""
-    if len(data) == 1:
-        return data[0]
-
-    position = nth(zip(*data), poss)
-    if position is None:
-        print('No solution found, terminating...')
-        exit(1)
-
-    bit_balance = reduce(calc_bit_balance, position, 0)
-    return filter_by_criteria(
-               list(filter(lambda b: criteria(b[poss], bit_balance), data)),
-               criteria,
-               poss + 1)
+    try:
+        groups = list((bit, list(g)) for bit, g in groupby(sorted(data), lambda l: l[poss]))
+    except IndexError:
+        return ''
+    if len(groups) == 1:
+        bit, group = groups[0]
+    elif criteria(groups[0][1], groups[1][1]):
+        bit, group = groups[1]
+    else:
+        bit, group = groups[0]
+    return bit + filter_by_criteria(group, criteria, poss + 1)
 
 
-def oxygen_criteria(position_bit: str, balance: int) -> bool: 
+def oxygen_criteria(zero_bits: list[str], one_bits: list[int]) -> bool: 
     """Criteria for a valid oxygen binary, given its current possitional bit
     and corresponding bit balance"""
-    return int(position_bit) == int(balance >= 0)
+    return len(one_bits) >= len(zero_bits)
 
 
-def co2_criteria(position_bit: str, balance: int) -> bool: 
+def co2_criteria(zero_bits: list[str], one_bits: list[int]) -> bool: 
     """Criteria for a valid co2 binary, given its current possitional bit
     and corresponding bit balance"""
-    return int(position_bit) == int(balance < 0)
+    return len(one_bits) < len(zero_bits)
 
 
-def calc_oxygen_co2_rate(data: list[str]) -> Tuple[int, int]:
+def calc_oxygen_co2_rate(data: list[str]) -> tuple[int, int]:
     """Given lines of binary string, returns the oxygen-, co2-rates as tuple."""
     oxygen_bit = filter_by_criteria(data, oxygen_criteria)
     co2_bit = filter_by_criteria(data, co2_criteria)
